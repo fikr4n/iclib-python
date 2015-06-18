da82c518a53313983caf0dfa8c52f346a1867924
root <fikr4n@gmail.com>
2015-06-18 07:44:38 +0700

    Add more example

--- example-hijri.py
+++ example-hijri.py
@@ -7,9 +7,21 @@ def ummqura_gregorian():
 	d = (1436, 1, 1)
 	print(d, '->', ummqura.to_gregorian(*d))
 
+def ummqura_gregorian_first():
+	print(ummqura.to_gregorian(1420, 1, 1))
+
+def ummqura_gregorian_last():
+	print(ummqura.to_gregorian(1450, 12, 30))
+
 def gregorian_ummqura():
 	d = (2015, 1, 1)
 	print(d, '->', ummqura.from_gregorian(*d))
+
+def gregorian_ummqura_first():
+	print(ummqura.from_gregorian(1999, 4, 17))
+
+def gregorian_ummqura_last():
+	print(ummqura.from_gregorian(2029, 5, 14))
 
 loc = sorted(locals().items())
 for k,v in loc:
--- example-qibla.py
+++ example-qibla.py
@@ -6,7 +6,6 @@ from iclib import qibla
 def istiqlal():
 	lat = -6.169777778
 	lng = 106.8307333
-	print('Masjid Istiqlal, Jakarta, Indonesia')
 	print(qibla.direction(lat, lng))
 	print(qibla.direction_dms(lat, lng))
 	print(qibla.direction_str(lat, lng))
@@ -14,7 +13,34 @@ def istiqlal():
 def yogyakarta():
 	lat = -(7 + 48 / 60.0)
 	lng = 110 + 21 / 60.0
-	print('Yogyakarta, Indonesia')
+	print(qibla.direction(lat, lng))
+	print(qibla.direction_dms(lat, lng))
+	print(qibla.direction_str(lat, lng))
+
+def nearnorthpole():
+	lat = 89
+	lng = 39.82616111
+	print(qibla.direction(lat, lng))
+	print(qibla.direction_dms(lat, lng))
+	print(qibla.direction_str(lat, lng))
+
+def nearsouthpole():
+	lat = -89
+	lng = 39.82616111
+	print(qibla.direction(lat, lng))
+	print(qibla.direction_dms(lat, lng))
+	print(qibla.direction_str(lat, lng))
+
+def makkah_east():
+	lat = 21.42250833
+	lng = 40
+	print(qibla.direction(lat, lng))
+	print(qibla.direction_dms(lat, lng))
+	print(qibla.direction_str(lat, lng))
+
+def makkah_west():
+	lat = 21.42250833
+	lng = 39
 	print(qibla.direction(lat, lng))
 	print(qibla.direction_dms(lat, lng))
 	print(qibla.direction_str(lat, lng))
